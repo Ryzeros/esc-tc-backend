5050d1b45ae0fa2bc9a1853759ea3728896d01de
youxiang <ryzeros@gmail.com>
2024-07-08 13:36:20 +0800

    get data by email

--- routers/credit.py
+++ routers/credit.py
@@ -27,3 +27,8 @@ async def add_item(item: CreditCreate, db: get_db = Depends()):
     result = CreditService(db).add_item(item)
     return handle_result(result)
 
+
+@router.post("/get_by_email", response_model=list[CreditItems])
+async def get_item(email: str, db: get_db = Depends()):
+    result = CreditService(db).get_items_by_email(email)
+    return handle_result(result)
