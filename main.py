4a0f946943ca35449009eb9415de98c5e8ce8893
xukaiteo <xukaiteo.work@gmail.com>
2024-07-08 14:52:03 +0800

    Fixed merge

--- main.py
+++ main.py
@@ -2,21 +2,12 @@ from fastapi import FastAPI
 from config.database import create_tables
 from routers.credit import router as credit_router
 from routers.loyalty import router as loyalty_router
-<<<<<<< HEAD
-=======
 from routers.promotions import router as promotion_router
->>>>>>> 5050d1b45ae0fa2bc9a1853759ea3728896d01de
 from fastapi.middleware.cors import CORSMiddleware
 
 create_tables()
 app = FastAPI()
 
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 5050d1b45ae0fa2bc9a1853759ea3728896d01de
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow requests from any origin
@@ -25,11 +16,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 5050d1b45ae0fa2bc9a1853759ea3728896d01de
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
